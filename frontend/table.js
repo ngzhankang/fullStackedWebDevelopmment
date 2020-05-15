62632ef0529dd8b057a78be24ee0184347706921
Ng Zhan Kang <64066238+ngzhankang@users.noreply.github.com>
2020-05-15 15:32:10 +0800

    Updates to index.html again. :(
    Co-authored-by: LOH CHERNG JUN TRISTON <TRISTONLOH.19@ichat.sp.edu.sg>

--- frontend/table.js
+++ frontend/table.js
@@ -1,18 +1,1 @@
-// Typewriter function
-var i = 0;
-var txt = "Lorem ipsum typing effect!"; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
-
-// Table paging method
-$(document).ready(function () {
-  $("#dtBasicExample").DataTable();
-  $(".dataTables_length").addClass("bs-select");
-});
+// Table filter method
