a2e3b0b1b76137b3dbbd045c372dad026a238978
Ng Zhan Kang <64066238+ngzhankang@users.noreply.github.com>
2020-05-18 17:22:33 +0800

    Edits to basic frontend result viewer.md

--- frontend/table.js
+++ frontend/table.js
@@ -1,1 +1,18 @@
 // Table filter method
+$(document).ready(function(){
+  $("#festivalIdInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#table tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+
+$(document).ready(function(){
+    $("#startTimeInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#table tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
