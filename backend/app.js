b436cc50402bf1d0d43575d41cd77e000866314d
Triston Loh <tristonloh.19@ichat.sp.edu.sg>
2020-07-01 00:22:30 +0800

    testtest

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// hello
 // // POST insert api for performance
 // app.post('/basic/insert/Performance', function (req, res, next) {
 //   const { data } = req.body;
