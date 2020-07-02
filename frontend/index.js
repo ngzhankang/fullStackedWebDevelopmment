10b13b027c87160776ab6dba71bcd1e8b653297f
Ng Zhan Kang <64066238+ngzhankang@users.noreply.github.com>
2020-07-02 22:29:05 +0800

    Edits to index.js

--- frontend/index.js
+++ frontend/index.js
@@ -67,10 +67,11 @@ function onBasicSwitch () {
   togglePopularityColumn();
   toggleAddSearch();
   togglePagination();
+  return true;
 }
 
 function SUMMONALLOUT () {
-  $("#basicSwitch").click(onBasicSwitch);
+  $("#basicSwitch").click(onBasicSwitch)
 }
 
 // call these functions
