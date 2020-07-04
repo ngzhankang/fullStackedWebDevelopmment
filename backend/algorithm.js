31a060c9d693457a8d8811b298ad8a5ebe1e1176
ngzhankang <zhankang.19@ichat.sp.edu.sg>
2020-07-04 23:51:51 +0800

    Some edits to algo.js(will continue tmr)

--- backend/algorithm.js
+++ backend/algorithm.js
@@ -5,16 +5,16 @@
 **/
 
 // try to catch error here!
-function compute (allThePerformances) {
+function compute (daEndTime) {
     try{
-        return { error: null, result: selectPerformanceByFestivalId(allThePerformances)}
+        return { error: null, result: sortPerformanceByFinishTime(daEndTime)}
     } catch (error) {
         return { error, result: null };
     }
 }
 
 // 1. selectPerformance to correctly select set of performance for computation
-function selectPerformanceByFestivalId(performances) {
+async function selectPerformanceByFestivalId(performances) {
     const l = performances.length;  //length of performances
     const selectedPerformance = []; //create a new array selectedPerformance
     for (let i = 0; i < l; i++) {   //iterate through all the festivalId
@@ -23,17 +23,19 @@ function selectPerformanceByFestivalId(performances) {
     return selectedPerformance; //return the array
 };
 
-// // 2. sortPerformance to sort performance by increasing order of their finishing time
-// const sortedSelectedPerformance = async function sortPerformanceByFinishTime(endTime) {
-//     const performanceSelected = await selectedPerformance;  //do a await to take the data from the selectedPerformance
-//     const storeArray = []   //create a new array to reorder the stuff again
-//     for (i = 1; i < performance.length; i++) {
-//         if (performanceSelected.endTime[i] < performanceSelected.endTime[i + 1]) {
-//             storeArray.push(performanceSelected.endTime[i]);// store it into a new array
-//         };
-//     };
-// };
+// 2. sortPerformance to sort performance by increasing order of their finishing time
+async function sortPerformanceByFinishTime(endTime) {
+    let filteredPerformance = await selectPerformanceByFestivalId;  //do a await to let the previous function execute first before continuing
+    const storeArray = []   //create a new array to reorder the stuff again
+    for (i = 0; i < filteredPerformance.length; i++) {
 
+        // if (filteredPerformance.endTime[i] < filteredPerformance.endTime[i + 1]) {
+        //     storeArray.push(filteredPerformance.endTime[i]);// store it into a new array
+        // };
+
+        console.log(filteredPerformance);
+    };
+};
 // // 3. selectedPerformances to maintain a list of selected performance
 // const selectedPerformances = [];
 
