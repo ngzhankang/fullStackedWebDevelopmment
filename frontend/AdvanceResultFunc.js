3ad84fe03c7dad42f4275eff538c8c58c77b1455
Triston Loh <tristonloh.19@ichat.sp.edu.sg>
2020-07-10 21:21:35 +0800

    finalise result viewer

--- frontend/AdvanceResultFunc.js
+++ frontend/AdvanceResultFunc.js
@@ -9,11 +9,10 @@ var currentData2 = []
 const advanceResultUrl = "http://localhost:3000/advance/result";
 
 // to populate the table upon getting the data from the backend
-function populateAdvanceResultTable(data, totalPopularity) {
+function populateAdvanceResultTable(data) {
   console.log(data);
-  console.log(totalPopularity)
-    const resultTableHtml = (data).map(
-      ({ performanceid, starttime, endtime, popularity }) => `
+  const resultTableHtml = (data).map(
+    ({ performanceid, starttime, endtime, popularity }) => `
             <tr>
                 <td>${performanceid}</td>
                 <td>${starttime}</td>
@@ -21,38 +20,43 @@ function populateAdvanceResultTable(data, totalPopularity) {
                 <td>${popularity}</td>
             </tr>
           `,
-    );
-    $('#advance-result-tbody').html(resultTableHtml);
-  }
+  );
+  $('#advance-result-tbody').html(resultTableHtml);
+}
 
-  // to populate the table upon getting the data from the backend
-function populateTotalPopularity(data, totalPopularity) {
-    console.log(data);
-    console.log(totalPopularity)
-      const resultsTableHtml = (data).map(
-        ({ totalPopularity }) => `
-              hi
-              ${"Total Popularity: " + totalPopularity}
+// to populate the total popularity upon getting the data from the backend
+function populateTotalPopularity(totalPopularity) {
+  const fakeArray = []
+  fakeArray.push(totalPopularity)
+  console.log(fakeArray);
+  const resultsTableHtml = (fakeArray).map(
+    ( totalPopularity ) => `
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><mark><strong>${"Total Popularity: " + totalPopularity}</strong></mark></td>
+        </tr>
             `,
-      );
-      $('#totalPopularity-result-tbody').html(resultsTableHtml);
-    }
+  );
+  $('#totalPopularity-result-tbody').html(resultsTableHtml);
+}
 
 
 // catch error from the backend and let the frontend handle it
 function catchDaErrorAdvance(data) {
-    if(data.error) {
-      alert(data.error)
-    }
-    else {  
-      populateAdvanceResultTable(data.result)
-      populateTotalPopularity(totalPopularity)
-    }
+  if (data.error) {
+    alert(data.error)
   }
+  else {
+    populateAdvanceResultTable(data.result)
+    populateTotalPopularity(data.totalPopularity)
+  }
+}
 
 // to fire the query to the backend and wait for response for table data
 function getAdvanceResultFromBackend(callback) {
-$.get(advanceResultUrl, advanceResultQuery)
+  $.get(advanceResultUrl, advanceResultQuery)
     .done((result) => callback(null, result))
     .fail((message) => callback(message, null));
 }
@@ -70,12 +74,12 @@ function refreshAdvanceResultTable() {
 // to compute the results that will be displayed after filter
 function computeAdvance() {
   $('#advance-result-input-form input')
-  .not(':input[type=submit]')
-  .each((_, input) => {
-    advanceResultQuery[$(input).attr('key')] = $(input).val();
-  });
-refreshAdvanceResultTable();
-return false;
+    .not(':input[type=submit]')
+    .each((_, input) => {
+      advanceResultQuery[$(input).attr('key')] = $(input).val();
+    });
+  refreshAdvanceResultTable();
+  return false;
 }
 
 // getting info from the frontend and then sending it to compute function
@@ -86,6 +90,6 @@ function registerAdvanceResultInput() {
 // call these functions
 $(document).ready(function () {
   registerAdvanceResultInput();
-//   refreshAdvanceResultTable();
+  //   refreshAdvanceResultTable();
 });
 
