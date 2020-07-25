b63cff3e2ef88cba407c5ca08d13c0bf8267e592
ngzhankang <zhankang.19@ichat.sp.edu.sg>
2020-07-25 13:07:15 +0800

    edits

--- backend/database.js
+++ backend/database.js
@@ -122,7 +122,7 @@ function resetAllTable(callback) {
         if (!festivalId && !startTime && !endTime) { whereClause = '' }
         // else if (!festivalId && startTime && endTime) {
         //     whereClause = 'WHERE'
-        //     whereClause += `startTime >= $${i++} AND endTime < $${i++}`;
+        //     whereClause += ` startTime >= $${i++} AND endTime < $${i++} `;
         //     values.push(parseInt(startTime));
         //     values.push(parseInt(endTime));
         // }
@@ -140,12 +140,12 @@ function resetAllTable(callback) {
                 whereClause += festivalId ? ` AND endTime < $${i++}` : ` endTime < $${i++}`;
                 values.push(parseInt(endTime));
             }
-            if (festivalId && startTime && endTime) {
-                whereClause += `festivalId = $${i++} AND starTime >= $${i++} AND endTime < $${i++}`;
-                values.push(parseInt(festivalId));
-                values.push(parseInt(startTime));
-                values.push(parseInt(endTime));
-            }
+            // if (festivalId && startTime && endTime) {
+            //     whereClause += `festivalId = $${i++} AND starTime >= $${i++} AND endTime < $${i++}`;
+            //     values.push(parseInt(festivalId));
+            //     values.push(parseInt(startTime));
+            //     values.push(parseInt(endTime));
+            // }
         }
         let limitoffsetClause = `LIMIT $${i++} OFFSET $${i++}`
         values.push(parseInt(pageSize));    //Limit = pageSize
